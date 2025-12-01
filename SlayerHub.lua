76be5dcecead93ffe42b12e761a0846e28bca331
FJN19-dev <josealbertsouzasantos189@gmail.com>
2025-11-30 22:57:18 -0300

    Add SlayerHub.lua to the project

---
+++ SlayerHub.lua
@@ -0,0 +1,4 @@
+-- 🔥 Loader oficial SlayerHub via Cloudflare Worker (sem key)
+-- Basta rodar o loadstring abaixo no Roblox Executor
+
+return loadstring(game:HttpGet("https://slayerhub.josealbertsouzasantos189.workers.dev/"))()
