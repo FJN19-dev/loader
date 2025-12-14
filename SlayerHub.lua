2b502ca5a83544c91f16c3a08b6bf646c11e5809
FJN19-dev <josealbertsouzasantos189@gmail.com>
2025-12-14 20:36:21 -0300

    Update print statement from 'Hello' to 'Goodbye'

--- SlayerHub.lua
+++ SlayerHub.lua
@@ -2491,22 +2491,20 @@ end)
 
 
 
--- Toggle
-local Toggle = Tabs.Main:AddToggle("AutoLevelFarm", { 
-    Title = "Auto Level", 
-    Default = false 
+Dropdown = Main:AddDropdown("DropdownFarm", {
+    Title = "Select Method Farm",
+    Values = {"Farm Level", "Farm Bone", "Farm Katakuri"},
+    Multi = false,
 })
-
-Toggle:OnChanged(function(Value)
-    getgenv().AutoFarm = Value
-    StopTween(getgenv().AutoFarm)
+Dropdown:SetValue("Farm Level")
+Dropdown:OnChanged(function(Value)
+FarmMode = Value
 end)
-
 spawn(function()
     local canRun = true
     local debounceTime = 0.5
     while wait(debounceTime) do
-        if getgenv().AutoFarm 
+        if getgenv().AutoFarm and FarmMode == "Farm Level" then
             if canRun then
                 canRun = false
                 spawn(function()
@@ -2567,4 +2565,190 @@ spawn(function()
             end
         end
     end
+end)
+local Bone = {
+    ["Reborn Skeleton"] = CFrame.new(-8769.58984, 142.13063, 6055.27637),
+    ["Living Zombie"] = CFrame.new(-10156.4531, 138.652481, 5964.5752),
+    ["Demonic Soul"] = CFrame.new(-9525.17188, 172.13063, 6152.30566),
+    ["Posessed Mummy"] = CFrame.new(-9570.88281, 5.81831884, 6187.86279)
+}
+spawn(function()
+    while task.wait(0.2) do
+        if getgenv().BonesBring then
+            pcall(function()
+                for _, v in ipairs(game.Workspace.Enemies:GetChildren()) do
+                    if Bone[v.Name] and v:FindFirstChild("HumanoidRootPart") and v:FindFirstChild("Humanoid") then
+                        v.HumanoidRootPart.CFrame = Bone[v.Name]
+                        v.Head.CanCollide = false
+                        v.Humanoid.Sit = false
+                        v.Humanoid:ChangeState(11)
+                        task.wait(0.1)
+                        v.Humanoid:ChangeState(14)
+                        v.HumanoidRootPart.CanCollide = false
+                        v.Humanoid.JumpPower = 0
+                        v.Humanoid.WalkSpeed = 0
+                        local animator = v.Humanoid:FindFirstChild("Animator")
+                        if animator then
+                            animator:Destroy()
+                        end
+                        sethiddenproperty(game.Players.LocalPlayer, "SimulationRadius", math.huge)
+                    end
+                end
+            end)
+        end
+    end
+end)
+BonePos = CFrame.new(-9506.234375, 172.130615234375, 6117.0771484375)
+spawn(function()
+    while wait(0.1) do
+        if FarmMode == "Farm Bone" and getgenv().AutoFarm and World3 then
+            pcall(function()
+                local enemies = game:GetService("Workspace").Enemies:GetChildren()
+                local foundEnemy = false
+                for _, v in pairs(enemies) do
+                    if v.Name == "Reborn Skeleton" or v.Name == "Living Zombie" or v.Name == "Demonic Soul" or v.Name == "Posessed Mummy" then
+                        if v:FindFirstChild("Humanoid") and v:FindFirstChild("HumanoidRootPart") and v.Humanoid.Health > 0 then
+                            foundEnemy = true
+                            repeat wait(0.1)
+                                AutoHaki()
+                                EquipWeapon(getgenv().SelectWeapon)
+                                v.HumanoidRootPart.CanCollide = false
+                                v.Humanoid.WalkSpeed = 0
+                                v.Head.CanCollide = false
+                                getgenv().BonesBring = true
+                                topos(v.HumanoidRootPart.CFrame * Pos)
+                            until not getgenv().AutoFarm or not v.Parent or v.Humanoid.Health <= 0
+                        end
+                    end
+                end                
+                if not foundEnemy then
+                    if BypassTP then
+                        local playerPos = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
+                        if (playerPos - BonePos.Position).Magnitude > 1500 then
+                            BTP(BonePos)
+                        else
+                            topos(BonePos)
+                        end
+                    else
+                        topos(BonePos)
+                    end
+                    UnEquipWeapon(getgenv().SelectWeapon)
+                    getgenv().BonesBring = false
+                    topos(CFrame.new(-9515, 164, 5786))                    
+                    for _, v in pairs(game:GetService("ReplicatedStorage"):GetChildren()) do
+                        if v.Name == "Reborn Skeleton" or v.Name == "Living Zombie" or v.Name == "Demonic Soul" or v.Name == "Posessed Mummy" then
+                            topos(v.HumanoidRootPart.CFrame * CFrame.new(2, 20, 2))
+                        end
+                    end
+                end
+            end)
+        end
+    end
+end)
+local Cake = {
+    ["Cookie Crafter"] = CFrame.new(-2333.28052, 37.8239059, -12093.2861),
+    ["Cake Guard"] = CFrame.new(-1575.56433, 37.8238907, -12416.2529),
+    ["Baking Staff"] = CFrame.new(-1872.35742, 37.8239517, -12899.4248),
+    ["Head Baker"] = CFrame.new(-2223.1416, 53.5283203, -12854.752)
+}
+spawn(function()
+    while task.wait(0.2) do
+        if getgenv().CakeBring then
+            pcall(function()
+                for _, v in pairs(game.Workspace.Enemies:GetChildren()) do
+                    if Cake[v.Name] then
+                        local targetCFrame = Cake[v.Name]
+                        if targetCFrame then
+                            v.HumanoidRootPart.CFrame = targetCFrame
+                        end
+                        v.Head.CanCollide = false
+                        v.Humanoid.Sit = false
+                        v.Humanoid:ChangeState(11)
+                        task.wait(0.1)
+                        v.Humanoid:ChangeState(14)
+                        v.HumanoidRootPart.CanCollide = false
+                        v.Humanoid.JumpPower = 0
+                        v.Humanoid.WalkSpeed = 0
+                        if v.Humanoid:FindFirstChild('Animator') then
+                            v.Humanoid.Animator:Destroy()
+                        end
+                        sethiddenproperty(game.Players.LocalPlayer, "SimulationRadius", math.huge)
+                    end
+                end
+            end)
+        end
+    end
+end)
+spawn(function()
+    while task.wait(0.1) do
+        if FarmMode == "Farm Katakuri" and getgenv().AutoFarm and World3 then
+            pcall(function()
+                game.ReplicatedStorage.Remotes.CommF_:InvokeServer("CakePrinceSpawner")                
+                if game.ReplicatedStorage:FindFirstChild("Cake Prince") or game:GetService("Workspace").Enemies:FindFirstChild("Cake Prince") then
+                    if game:GetService("Workspace").Enemies:FindFirstChild("Cake Prince") then
+                        for _, v in pairs(game.Workspace.Enemies:GetChildren()) do
+                            if getgenv().AutoFarm and v.Name == "Cake Prince" 
+                                and v:FindFirstChild("HumanoidRootPart") 
+                                and v:FindFirstChild("Humanoid") 
+                                and v.Humanoid.Health > 0 then                                
+                                repeat 
+                                    game:GetService("RunService").Heartbeat:wait()
+                                    AutoHaki()
+                                    EquipWeapon(getgenv().SelectWeapon)
+                                    v.HumanoidRootPart.CanCollide = false
+                                    v.Humanoid.WalkSpeed = 0
+                                    v.HumanoidRootPart.Size = Vector3.new(60, 60, 60)
+                                    topos(v.HumanoidRootPart.CFrame * Pos)
+                                until not getgenv().AutoFarm or not v.Parent or v.Humanoid.Health <= 0
+                            end
+                        end
+                    else
+                        if game:GetService("Workspace").Map.CakeLoaf.BigMirror.Other.Transparency == 0 
+                            and (CFrame.new(-1990.672607421875, 4532.99951171875, -14973.6748046875).Position 
+                            - game.Players.LocalPlayer.Character.HumanoidRootPart.Position).Magnitude >= 2000 then                            
+                            topos(CFrame.new(-2151.82153, 149.315704, -12404.9053))
+                        end
+                    end
+                else
+                    if game:GetService("Workspace").Enemies:FindFirstChild("Cookie Crafter") 
+                        or game:GetService("Workspace").Enemies:FindFirstChild("Cake Guard") 
+                        or game:GetService("Workspace").Enemies:FindFirstChild("Baking Staff") 
+                        or game:GetService("Workspace").Enemies:FindFirstChild("Head Baker") then                        
+                        for _, v in pairs(game.Workspace.Enemies:GetChildren()) do
+                            if v:FindFirstChild("Humanoid") and v:FindFirstChild("HumanoidRootPart") and v.Humanoid.Health > 0 then
+                                if (v.Name == "Cookie Crafter" or v.Name == "Cake Guard" 
+                                    or v.Name == "Baking Staff" or v.Name == "Head Baker") 
+                                    and v:FindFirstChild("HumanoidRootPart") 
+                                    and v:FindFirstChild("Humanoid") 
+                                    and v.Humanoid.Health > 0 then                                    
+                                    repeat 
+                                        game:GetService("RunService").Heartbeat:wait()
+                                        AutoHaki()
+                                        EquipWeapon(getgenv().SelectWeapon)
+                                        v.HumanoidRootPart.CanCollide = false
+                                        v.Humanoid.WalkSpeed = 0
+                                        v.Head.CanCollide = false
+                                        topos(v.HumanoidRootPart.CFrame * Pos)
+                                        getgenv().CakeBring = true
+                                    until not getgenv().AutoFarm or not v.Parent or v.Humanoid.Health <= 0
+                                end
+                            end
+                        end
+                    else
+                        local CakePos = CFrame.new(-2077, 252, -12373)
+                        if BypassTP then
+                            BTP(CakePos)
+                        else
+                            topos(CakePos)
+                        end
+                    end
+                end
+            end)
+        end
+    end
+end)
+Toggle = Main:AddToggle("Toggle", { Title = "Start Farm", Default = false })
+Toggle:OnChanged(function(Value)
+    getgenv().AutoFarm = Value
+    StopTween(getgenv().AutoFarm)
 end)
